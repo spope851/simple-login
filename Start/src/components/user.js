e0e3a473a84f45efaa59f0c6323ca118e186b2c2
Spencer Pope <34104632+spope851@users.noreply.github.com>
2020-09-19 14:38:15 -0400

    Update user.js

--- Start/src/components/user.js
+++ Start/src/components/user.js
@@ -4,20 +4,12 @@ class User extends React.Component{
 
   
   render(){
-    const {username} = this.props
-    const message =
-      `Logged in as:  
-      ${username? 
-        username 
-          : 
-        'Guest'}, Welcome!`
+
+    
+    
     return (
       <div className="card text-dark">
-        {message} 
-        {username ? 
-          ''
-            :
-          <span>Click <a href='/login'>here</a> to sign up for an account</span>}
+        
       </div>
     );
   }
